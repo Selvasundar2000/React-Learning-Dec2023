1a56e004926673c81789294fac2575a7835fb969
Selvasundar2000 <selvasundarrgs@gmail.com>
2023-12-21 17:40:02 +0530

    5:39 pm

--- react03tailwindprops/src/components/JobRole.jsx
+++ react03tailwindprops/src/components/JobRole.jsx
@@ -1,5 +1,5 @@
 const JobRoles = [
-  { id: 1,name:'Selva Sundar', title: 'img1', description: 'junior developer' },
+  { id: 1,name:'Selva Sundar', title: 'img1', description: 'Junior developer' },
   { id: 2,name:'Santhosh', title: 'img2', description: 'Associate developer' },
   { id: 3,name:'Suresh', title: 'img3', description: 'Senior developer' }
 ]
