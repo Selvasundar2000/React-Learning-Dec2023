cd95ee9b8748034ebc0a7a69c86155bb04c522fd
Selvasundar2000 <selvasundarrgs@gmail.com>
2024-02-23 12:57:21 +0530

    hello

--- react07Router/src/components/MockAPI/MockAPI.jsx
+++ react07Router/src/components/MockAPI/MockAPI.jsx
@@ -1,58 +1,47 @@
-import React, { useEffect } from 'react'
+ import React, { useEffect,useState } from 'react'
+import jsonData from '../MockAPI/DummyAPI'
 
-function MockAPI() {
 
-    const [d, setData] = React.useState([]);
+export default function MockAPI() {
+    const [post, getPost] = useState([])
+    const API = 'https://65ba309db4d53c0665524358.mockapi.io/SelvaTest/JOB_ROLE';
+    const fetchPost = () => {
+      fetch(API)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res)
+          getPost(res)
+        })
+    }
     useEffect(() => {
-
-        fetch('https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001')
-            .then((res) => res.json())
-            .then((d) => {
-                console.log(d)
-                setData(d)
-            })
-
-    }, [])
-
-    return (
-
-
-        <>
-            {d.map((d) =>
-                <div class="relative overflow-x-auto">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-
-                            <tr >
-
-                                <th>ID</th>
-                                <th>First Name</th>
-                                <th width="300">Email</th>
-                                <th>Phone number</th>
-
-                            </tr>
-                        </thead>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-
-                            <td class="px-6 py-4">{d.id}</td>
-                            <td class="px-6 py-4">{d.firstName}</td>
-                            <td class="px-6 py-4"> {d.email}</td>
-                            <td class="px-6 py-4">{d.contactNumber}</td>
-
-                        </tr>
-                    </table>
-                </div>
-            )}
-        </>
-
-
-
-    )
+      fetchPost()
+    }, [post])
+  return (
+    <>
+     <div class="relative overflow-x-auto ">
+     
+      <table  class="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border-collapse border border-slate-400 ">
+       
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th scope="col" class="px-6 py-3">ID</th>
+        <th scope="col" class="px-6 py-3">Name</th>
+        <th scope="col" class="px-6 py-3">Role</th>
+        <th scope="col" class="px-6 py-3">Location</th>
+        
+        </tr>
+        
+      
+        {post.map((item, i) => {
+          return( <tr class="bg-white dark:bg-gray-800 hover:bg-blue-100 border-collapse border border-slate-400" key={i}>{item.id}
+         <td class="px-6 py-4 "> {item.Name}</td>
+          <td class="px-6 py-4">{item.Role}</td>
+          <td class="px-6 py-4">{item.Location} </td>
+          </tr>
+          )
+        })}
+      </table>
+     
+      </div>
+    </>
+  )
 }
-
-
-
-
-
-
-export default MockAPI
